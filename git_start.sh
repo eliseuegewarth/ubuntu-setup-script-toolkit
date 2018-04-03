08a7e346674724ca798e9306a0601e6b6a246620
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-04-03 19:17:49 -0300

    Adicionando script de clone de repos default
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

---
+++ git_start.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Config git user defaults
+git config --local core.commentChar $GIT_COMMENT;
+git config --global user.email $USER_EMAIL;
+git config --global user.name "${USER_NAME}";
+
+
+# Cheching for ssh keys
+if [ -f $HOME/.ssh/id_rsa ]; then
+	GIT_PROTOCOL='git@github.com:';
+else
+	GIT_PROTOCOL='https://github.com/';
+fi
+
+cd $WORKSPACE && \
+git clone ${GIT_PROTOCOL}interlegis/sapl.git --branch master --single-branch;
+git clone ${GIT_PROTOCOL}fga-gpp-mds/agr-react-native.git --branch master --single-branch;
+git clone ${GIT_PROTOCOL}fga-gpp-mds/agr-gic-api.git --branch master --single-branch;
