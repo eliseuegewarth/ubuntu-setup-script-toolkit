975995b0536d77a63ada73790cab9c10a8298883
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-04-03 17:18:28 -0300

    Gerando script de configuração de ssh
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- ssh_config.sh
+++ ssh_config.sh
@@ -5,6 +5,7 @@ if [ ! -f $HOME/.ssh/id_rsa ]; then
 	ssh-keygen -t rsa -b 4096 -N $PASSWORD -C "${GIT_USER_EMAIL}" -f $HOME/.ssh/id_rsa
 	eval "$(ssh-agent -s)"
 	ssh-add ~/.ssh/id_rsa;
+	echo "Chave gerada e adicionada ao ssh-agent. Adicione a chave ao seu repositório."
 else
 	echo "Já existe uma chave SSH configurada."
 fi
