3b7add357fd1a3474262a5c19ad9c341641d202c
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-04-03 18:06:16 -0300

    Atualizando download_android_studio.sh
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- download_android_studio.sh
+++ download_android_studio.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-wget https://dl.google.com/dl/android/studio/ide-zips/3.1.0.16/android-studio-ide-173.4670197-linux.zip -o /tmp/android-studio.zip && \
-cd $HOME && unzip /tmp/android-studio.zip
-
 # adicional libs for android studio
-sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
+sudo apt-get -qq -y install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
+
+# Instalação Android Studio
+wget https://dl.google.com/dl/android/studio/ide-zips/3.1.0.16/android-studio-ide-173.4670197-linux.zip -o /tmp/android-studio.zip && \
+unzip /tmp/android-studio.zip
+./android-studio/bin/studio.sh
