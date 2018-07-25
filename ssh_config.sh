71dc4bb6377763cdb584a0f33e5ff17cb0b85f80
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-07-25 19:54:18 -0300

    Atualizando script de configuração ssh
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- ssh_config.sh
+++ ssh_config.sh
@@ -3,9 +3,11 @@
 if [ ! -f $HOME/.ssh/id_rsa ]; then
 	export REPO_PATH=$(git rev-parse --show-toplevel);
 	source ${REPO_PATH}/.secret_env
-	ssh-keygen -t rsa -b 4096 -N $PASSWORD -C "${GIT_USER_EMAIL}" -f $HOME/.ssh/id_rsa
+	ssh-keygen -t rsa -b 4096 -N "${PASSWORD}" -C "${GIT_USER_EMAIL}" -f "${HOME}/.ssh/id_rsa"
+	echo "Chave configurada"
 	eval "$(ssh-agent -s)"
-	ssh-add ~/.ssh/id_rsa;
+	echo "Chave configurada"
+	ssh-add -N "${PASSWORD}"  ~/.ssh/id_rsa;
 	echo "Chave gerada e adicionada ao ssh-agent. Adicione a chave ao seu repositório."
 else
 	echo "Já existe uma chave SSH configurada."
