35d9dcd827c74e3546ff8af1cb6d5658f74252e4
Eliseu Egewarth <eliseuegewarth@gmail.com>
2019-04-29 11:21:20 -0300

    Corrigindo configuracao do install_zsh.sh
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
 sudo apt -qq -y install zsh && \
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" && \
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="avit"/g' ~/.zshrc && \
+# General bash setup
+if [ -z "$(cat ${HOME}/.zshrc | grep bash_env)" ]; then
+    echo "Adicionando bash_env em zshrc ..." && \
+    echo -e "\nif [ -f ${HOME}/.bash_env ]; then\n    . ${HOME}/.bash_env\nfi" >> ${HOME}/.zshrc;
+fi
+
+if [ -z "$(cat ${HOME}/.zshrc | grep bash_aliases)" ]; then
+    echo "Adicionando bash_aliases em zshrc ..." && \
+    echo -e "\nif [ -f ${HOME}/.bash_aliases ]; then\n    . ${HOME}/.bash_aliases\nfi" >> ${HOME}/.zshrc;
+fi
+
 sudo sed -i 's|${HOME}:/usr/bin/bash|${HOME}:/usr/bin/zsh|g' /etc/passwd
