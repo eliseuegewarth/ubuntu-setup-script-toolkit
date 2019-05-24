410edb133dbe916d7a67a9740966f70683d8a0e4
Eliseu Egewarth <eliseuegewarth@gmail.com>
2019-05-23 21:49:11 -0300

    Atualizando parametro do script apt_automated_updates.sh
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- apt_automated_updates.sh
+++ apt_automated_updates.sh
@@ -1,8 +1,15 @@
-complement=""
-if [ ! -z $1 ]
+complement="echo 'Finished'";
+if [ -z $1 ]
 then
-	complement=" && $1";
+	echo "nothing to do";
+else
+	param_number_limit=5;
+	if [ $# -gt $param_number_limit ]
+	then
+		echo "The limit of parameter is $param_number_limit."
+		exit 0;
+	else
+		complement="$*";
+	fi
 fi
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean $complement && exit 0 || exit 1
-
-
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean $(${complement}) && exit 0 || exit 1
