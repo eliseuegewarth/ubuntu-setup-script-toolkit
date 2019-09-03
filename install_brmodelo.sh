69dca348127f4cf4bc3509cae4aff1cb11d00568
Eliseu Egewarth <eliseuegewarth@gmail.com>
2019-09-03 19:55:46 -0300

    Removing 'sudo' from script
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- install_brmodelo.sh
+++ install_brmodelo.sh
@@ -1,27 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 brmodelo_path="/opt/brmodelo"
-sudo rm -rf ${brmodelo_path}/ && sudo mkdir -p ${brmodelo_path}/
-sudo chmod 777 ${brmodelo_path}
-sudo wget http://www.sis4.com/brModelo/brModelo.jar -O ${brmodelo_path}/brModelo.jar
-sudo wget https://github.com/chcandido/brModelo/raw/master/src/imagens/icone.png -O ${brmodelo_path}/brmodelo.png
+rm -rf ${brmodelo_path}/ && mkdir -p ${brmodelo_path}/
+chmod 777 ${brmodelo_path}
+wget http://www.sis4.com/brModelo/brModelo.jar -O ${brmodelo_path}/brModelo.jar
+wget https://github.com/chcandido/brModelo/raw/master/src/imagens/icone.png -O ${brmodelo_path}/brmodelo.png
 
 brmodelo_app_file="${HOME}/.local/share/applications/brmodelo.desktop"
-sudo touch "${brmodelo_path}/brmodelo.sh"
-echo "(cd ${brmodelo_path}/ && java -jar brModelo.jar)&" | sudo tee "${brmodelo_path}/brmodelo.sh"
-sudo chmod +x "${brmodelo_path}/brmodelo.sh"
+touch "${brmodelo_path}/brmodelo.sh"
+echo "(cd ${brmodelo_path}/ && java -jar brModelo.jar)&" | tee "${brmodelo_path}/brmodelo.sh"
+chmod +x "${brmodelo_path}/brmodelo.sh"
 
-sudo echo "[Desktop Entry]" > ${brmodelo_app_file}
-sudo echo "Encoding=UTF-8"  >> ${brmodelo_app_file}
-sudo echo "Version=3.2"  >> ${brmodelo_app_file}
-sudo echo "Terminal=false"  >> ${brmodelo_app_file}
-sudo echo "Comment=BRModelo 3.2"  >> ${brmodelo_app_file}
-sudo echo "Name=BRModelo"  >> ${brmodelo_app_file}
-sudo echo "Exec=${brmodelo_path}/brmodelo.sh"  >> ${brmodelo_app_file}
-sudo echo "Icon=${brmodelo_path}/brmodelo.png"  >> ${brmodelo_app_file}
-sudo echo "StartupWMClass=brmodelo"  >> ${brmodelo_app_file}
-sudo echo "Type=Application"  >> ${brmodelo_app_file}
-sudo echo "Categories=Modeling;"  >> ${brmodelo_app_file}
-sudo echo "MimeType=x-scheme-handler/tg;"  >> ${brmodelo_app_file}
-sudo echo "X-Desktop-File-Install-Version=0.22"  >> ${brmodelo_app_file}
+echo "[Desktop Entry]" > ${brmodelo_app_file}
+echo "Encoding=UTF-8"  >> ${brmodelo_app_file}
+echo "Version=3.2"  >> ${brmodelo_app_file}
+echo "Terminal=false"  >> ${brmodelo_app_file}
+echo "Comment=BRModelo 3.2"  >> ${brmodelo_app_file}
+echo "Name=BRModelo"  >> ${brmodelo_app_file}
+echo "Exec=${brmodelo_path}/brmodelo.sh"  >> ${brmodelo_app_file}
+echo "Icon=${brmodelo_path}/brmodelo.png"  >> ${brmodelo_app_file}
+echo "StartupWMClass=brmodelo"  >> ${brmodelo_app_file}
+echo "Type=Application"  >> ${brmodelo_app_file}
+echo "Categories=Modeling;"  >> ${brmodelo_app_file}
+echo "MimeType=x-scheme-handler/tg;"  >> ${brmodelo_app_file}
+echo "X-Desktop-File-Install-Version=0.22"  >> ${brmodelo_app_file}
 
 echo "Finished Setup..."
