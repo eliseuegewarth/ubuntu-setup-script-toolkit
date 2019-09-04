159606ee98c5e6e4b47b6d30014aab5ea9d8e5f0
Eliseu Egewarth <eliseuegewarth@gmail.com>
2019-09-03 23:59:09 -0300

    Adicionando comando para apagar arquivos existentes
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- sublime_text/post_config.sh
+++ sublime_text/post_config.sh
@@ -4,16 +4,18 @@ REPO_PATH="$(dirname "$0")"
 cd ${REPO_PATH}
 # Install Package Control for Sublime Text 3
 SUBLIME_CONFIG_PATH="${HOME}/.config/sublime-text-3"
-rm -rf ${SUBLIME_CONFIG_PATH}/Installed\ Packages/ && \
+rm -rf ${SUBLIME_CONFIG_PATH}/Installed\ Packages/;
 mkdir -p ${SUBLIME_CONFIG_PATH}/Installed\ Packages/ && \
 cd ${SUBLIME_CONFIG_PATH}/Installed\ Packages/ && \
 wget https://packagecontrol.io/Package%20Control.sublime-package && \
+rm -rf ${SUBLIME_CONFIG_PATH}/Packages;
 mkdir -p ${SUBLIME_CONFIG_PATH}/Packages && \
 cd ${SUBLIME_CONFIG_PATH}/Packages && \
 git clone https://bitbucket.org/hmml/jsonlint.git --single-branch --branch master && \
 git clone https://github.com/djjcast/mirodark-st2 --single-branch --branch master && \
 git clone --single-branch --branch master https://github.com/eliseuegewarth/material-theme.git Material\ Theme && \
 git clone --single-branch --branch master https://github.com/eliseuegewarth/material-theme-appbar.git Material\ Theme\ -\ Appbar && \
+rm -rf ${SUBLIME_CONFIG_PATH}/Packages/User;
 mkdir -p ${SUBLIME_CONFIG_PATH}/Packages/User && \
 cd ${SUBLIME_CONFIG_PATH}/Packages && \
 cp ${REPO_PATH}/Preferences.sublime-settings Preferences.sublime-settings
