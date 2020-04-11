949733c9ef5b573a8861f9b264e85f47d26e0370
Eliseu Egewarth <eliseuegewarth@gmail.com>
2020-04-11 15:31:05 -0300

    Updating bash variables
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- install_pip_packages.sh
+++ install_pip_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "pip install..." && \
-sudo apt -qq -y install python-pip && \
+sudo apt -qq -y install python3-pip && \
 sudo -H pip install --upgrade pip && \
 sudo -H pip install --upgrade setuptools && \
 sudo -H pip install virtualenv virtualenvwrapper ipython ipdb && exit 0 || exit 1;
