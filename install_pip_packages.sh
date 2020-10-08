16de99863770cd145dbbdd5d691df13423141f50
Eliseu Egewarth <eliseuegewarth@gmail.com>
2020-10-08 12:12:42 -0300

    Updating pip version
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- install_pip_packages.sh
+++ install_pip_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "pip install..." && \
 sudo apt -qq -y install python3-pip && \
-sudo -H pip install --upgrade pip && \
-sudo -H pip install --upgrade setuptools && \
-sudo -H pip install virtualenv virtualenvwrapper ipython ipdb && exit 0 || exit 1;
+sudo -H pip3 install --upgrade pip && \
+sudo -H pip3 install --upgrade setuptools && \
+sudo -H pip3 install --use-feature=2020-resolver virtualenv virtualenvwrapper ipython ipdb && exit 0 || exit 1;
